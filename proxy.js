9d8530239cf16443da95708110e13202d6f252f6
Nathan Vander Wilt <nate@calftrail.com>
2014-12-31 10:32:52 -0600

    use tunnel-level messages for auth (and greatly simplify codebase)

--- proxy.js
+++ proxy.js
@@ -1,10 +1,47 @@
-var proxy = require('./lib/proxy-server'),
-    token = require('./lib/token-server');
+var net = require('net'),     // TODO: tls
+    streamplex = require('streamplex');
 
-proxy.ProxyServer().listen(5005, function(){
+net.createServer(function (tunnelSocket) {
+  console.log("client opened tunnel socket");
+  
+  var tunnel = streamplex(streamplex.A_SIDE),
+      authed = false;
+  tunnelSocket.pipe(tunnel).pipe(tunnelSocket);
+  tunnel.on('message', function (d) {
+    if (d.token === 'DEV-CRED') authed = true;
+    tunnel.sendMessage({authed:authed});
+    console.log("client authenticated:", authed);
+  });
+  tunnel.on('stream', function (stream, type) {
+    if (!authed) {
+      console.warn("Unauthorized stream request!");
+      tunnel.sendMessage({error:"Not authorized!"});
+      return tunnelSocket.destroy();
+    }
+    console.log("client created a stream");
+    
+    var SocketClass = (type === 'tls') ? tls.TLSSocket : net.Socket,
+        socket = new SocketClass();
+    stream.on('_pls_connect', function (port, host) {
+      console.log("connection request:", host, port);
+      // TODO: how to prevent unwanted (i.e. LAN/loopback) outbound?
+      socket.connect(port, host, function () {
+        console.log("connection success");
+        if (type === 'tls') stream.remoteEmit('secureConnect');
+        else stream.remoteEmit('connect');
+        stream.pipe(socket).pipe(stream);
+      }).on('error', function (e) {
+        stream.emit('error', e);
+        stream.destroy();
+      });
+    });
+    stream.on('_pls_timeout', function (msecs) {
+      socket.setTimeout(msecs);
+    });
+    socket.on('timeout', function () {
+      stream.remoteEmit('timeout');
+    });
+  });
+}).listen(5005, function(){
   console.log('Proxy server listening on port', this.address().port);
-});
-
-token.TokenServer().listen(5006, function(){
-  console.log('Token server listening on port', this.address().port);
 });
